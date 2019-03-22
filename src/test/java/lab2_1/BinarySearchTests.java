dc39a416ad857d78c4f71a63d199a02ba1472591
Dominik <rakdominikrak@gmail.com>
2019-03-22 09:09:00 +0100

    testy_v1 shouldSayThatElementIsNotFoundAndReturnIndexMinusOne()

--- src/test/java/lab2_1/BinarySearchTests.java
+++ src/test/java/lab2_1/BinarySearchTests.java
@@ -1,6 +1,7 @@
 package lab2_1;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -18,5 +19,13 @@ public class BinarySearchTests {
         SearchResult searchResult = BinarySearch.search(elementToFind, tabOfLengthOne);
         assertTrue(searchResult.isFound());
         assertEquals(elementToFind, tabOfLengthOne[searchResult.getPosition()]);
+    }
+
+    @Test
+    public void shouldSayThatElementIsNotFoundAndReturnIndexMinusOne() {
+        int elementToFind = 1;
+        SearchResult searchResult = BinarySearch.search(elementToFind, tabOfLengthOne);
+        assertFalse(searchResult.isFound());
+        assertEquals(-1, searchResult.getPosition());
     }
 }
